763be997467bdbdc447d043b92125c3c1645a389
Joel Dankert <joel.dankert@gmail.com>
2026-02-03 21:13:24 +0100

    works nicely

--- hostcontrol/sounds/.process_raw.sh
+++ hostcontrol/sounds/.process_raw.sh
@@ -35,7 +35,7 @@ for in_file in "${files[@]}"; do
 
   if ffmpeg -hide_banner -loglevel error -y \
       -i "$in_file" \
-      -af "loudnorm=I=-16:TP=-1.5:LRA=11,silenceremove=start_periods=1:start_silence=0.15:start_threshold=-55dB:stop_periods=1:stop_silence=0.40:stop_threshold=-55dB" \
+      -af "loudnorm=I=-16:TP=-1.5:LRA=11,silenceremove=start_periods=1:start_silence=0:start_threshold=-60dB,areverse,silenceremove=start_periods=1:start_silence=0:start_threshold=-60dB,areverse" \
       -ar 48000 -ac 2 -b:a 192k \
       "$tmp_out"; then
     mv -f "$tmp_out" "$OUT_DIR/$name"
